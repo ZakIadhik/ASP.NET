6a71294c0959db2ba23f7648734a0304fd1dba89
ZakIadhik <nazarov.ramin.2004@mail.ru>
2025-07-30 20:43:47 +0400

    Добавлен проект StudentManagementApi в мой репозиторий

---
+++ Desktop/ASP.NET/StudentManagementApi/StudentData/DTOs/CreateStudentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentData.DTOs
+{
+    public class CreateStudentDTO
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ Desktop/ASP.NET/StudentManagementApi/StudentData/Mapping/StudentProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StudentData.Models;
+using StudentData.DTOs;
+
+namespace StudentData.Mapping
+{
+    public class StudentProfile : Profile
+    {
+        public StudentProfile()
+        {
+            CreateMap<CreateStudentDTO, Student>();
+
+            CreateMap<Student, StudentDTO>()
+                .ForMember(dest => dest.FullName,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => DateTime.Now.Year - src.BirthDate.Year));
+        }
+    }
+}
